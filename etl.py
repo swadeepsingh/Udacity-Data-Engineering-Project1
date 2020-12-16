191e3c00b157385ccfa66518111de2eb19595a14
Swadeep Singh <swadeepiitkgp@gmail.com>
2020-12-16 10:52:32 +0530

    added explanations to functions

--- etl.py
+++ etl.py
@@ -6,6 +6,17 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    """
+    This function perform ETL on the first dataset, song_data, 
+    to create the songs and artists dimensional tables.
+    
+    Arguments:
+        cur: the sursor object
+        filepath: song data (JSON) file path
+    
+    Returns:
+        None
+    """
     # open song file
     df = pd.read_json(filepath, lines=True)
 
@@ -19,6 +30,18 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+    """
+    This function perform ETL on the second dataset, log_data, 
+    to create the time and users dimensional tables, 
+    as well as the songplays fact table.
+    
+    Arguments:
+        cur: the sursor object
+        filepath: log data (JSON) file path
+    
+    Returns:
+        None
+    """
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -65,6 +88,20 @@ def process_log_file(cur, filepath):
 
 
 def process_data(cur, conn, filepath, func):
+   """
+    Description: This function is responsible for listing the files in a directory,
+    and then executing the ingest process for each file according to the function
+    that performs the transformation to save it to the database.
+
+    Arguments:
+        cur: the cursor object.
+        conn: connection to the database.
+        filepath: log data or song data file path.
+        func: function that transforms the data and inserts it into the database.
+
+    Returns:
+        None
+    """
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
